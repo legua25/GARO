72660a27d020601cf35af706d00cceaf892e1f79
estebanquintana6 <a01184855@itesm.mx>
2017-02-13 18:21:48 -0600

    Video added

--- js/maps.js
+++ js/maps.js
@@ -1,24 +1,51 @@
 function initMap() {
   var myLatLng = {lat: 20.59111, lng: -100.39111};
-  var myLatLng2 = {lat: 20.614728, lng: -100.3894137};
+  var parqueqro2000 = {lat: 20.617167, lng:  -100.401361};
+  var parqueqro20002 = {lat: 20.617000, lng: -100.396500};
+  var cuotacelaya = {lat: 20.576806, lng: -100.408306};
+  var surponiente = {lat: 20.550194, lng: -100.374222};
+  var zaragoza = {lat: 20.580444, lng: -100.409778};
+  
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
     center: myLatLng
   });
 
   var image = 'img/spectacular.png';
+    
   var marker = new google.maps.Marker({
-    position: myLatLng,
+    position: parqueqro2000,
     map: map,
     icon: image,  
-    title: 'Hello World!'
+    title: 'Parque Querértaro'
   });
     
   var marker2 = new google.maps.Marker({
-    position: myLatLng2,
+    position: parqueqro20002,
     map: map,
     icon: image,
-    title: 'Hello World2!'
+    title: 'Parque Querétaro 2'
+  });
+    
+    var marker3 = new google.maps.Marker({
+    position: cuotacelaya,
+    map: map,
+    icon: image,
+    title: 'Carretera cuota Celaya'
+  });
+    
+    var marker4 = new google.maps.Marker({
+    position: surponiente,
+    map: map,
+    icon: image,
+    title: 'Libramiento surponiente'
+  });
+    
+    var marker5 = new google.maps.Marker({
+    position: zaragoza,
+    map: map,
+    icon: image,
+    title: 'Prolongación Zaragoza'
   });
     
 }
---
+++ js/video.js
@@ -0,0 +1,23 @@
+/** Document Ready Functions **/
+/********************************************************************/
+
+$( document ).ready(function() {
+
+    // Resive video
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+        
+    $(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+/** Reusable Functions **/
+/********************************************************************/
